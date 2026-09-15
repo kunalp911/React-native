67e830e0ebaa0965dbb203a5ecf98be8fbd8e663
kunalp911 <patilkunal911139@gmail.com>
2026-09-15 22:18:43 +0530

    text change

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ const App = (): React.JSX.Element => {
             React Native App new app
           </Text>
           <Text>
-            Welcome to your React Native application!
+            Welcome kunal!
           </Text>
         </View>
       </View>
